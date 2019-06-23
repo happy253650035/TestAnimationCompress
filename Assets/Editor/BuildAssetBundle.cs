eb18cde29012d21b3c100af09a3648aee4b6488e
yinyi <yinyi@kuaishou.com>
2019-06-23 13:25:04 +0800

    上传测试工程

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class BuildAssetBundle 
+{
+	[MenuItem ("Tools/BuildAnimationCompress")]
+	static void BuildAnimationCompress()
+	{
+		BuildAb("Assets/AniCompress.prefab");
+	}
+	
+	[MenuItem ("Tools/BuildAnimationUnCompress")]
+	static void BuildAnimationUnCompress()
+	{
+		BuildAb("Assets/AniUnCompress.prefab");
+	}
+	
+	[MenuItem ("Tools/BuildAnimationFakeCompress")]
+	static void BuildAnimationFakeCompress()
+	{
+		BuildAb("Assets/AniFakeCompress.prefab");
+	}
+	
+	[MenuItem ("Tools/BuildAnimationTrueCompress")]
+	static void BuildAnimationTrueCompress()
+	{
+		BuildAb("Assets/AniTrueCompress.prefab");
+	}
+
+	static void BuildAb(string assetPath)
+	{
+		List<AssetBundleBuild> list = new List<AssetBundleBuild>();
+		Object assetOb = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+		string path = AssetDatabase.GetAssetPath(assetOb);
+		string[] needList = AssetDatabase.GetDependencies(new[] {path});
+		foreach (string needPath in needList)
+		{
+			if (!needPath.Contains(".cs"))
+			{
+				AssetBundleBuild build = new AssetBundleBuild();
+				string[] namesList = needPath.Split (new[]{ "." }, StringSplitOptions.RemoveEmptyEntries);
+				build.assetBundleName = namesList[0];
+				build.assetNames = new[] {needPath};
+				list.Add(build);
+			}
+		}
+
+		string filePath = Directory.GetCurrentDirectory() + "/AssetBundle";
+		if (!Directory.Exists(filePath))
+		{
+			Directory.CreateDirectory(filePath);
+		}
+
+		BuildPipeline.BuildAssetBundles(Directory.GetCurrentDirectory() + "/AssetBundle", list.ToArray(),
+			BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle,
+			BuildTarget.Android);
+	}
+}
---
+++ Assets/Editor/ToolAnimationCompress.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class ToolAnimationCompress : MonoBehaviour {
+	[MenuItem("Tools/AnimationTool/CompressClip")]
+	private static void Execute()
+	{
+		int number = 0;
+		DateTime time = DateTime.Now;
+		foreach (UnityEngine.Object o in Selection.GetFiltered(typeof(AnimationClip), SelectionMode.DeepAssets))
+		{
+			number++;
+			ForFun(Instantiate(o) as AnimationClip, AssetDatabase.GetAssetPath(o));
+		}
+		AssetDatabase.SaveAssets();
+		Debug.Log("一共压缩了"+number+"个动画文件!");
+		Debug.Log("耗时:"+(DateTime.Now-time).TotalMilliseconds/1000+"秒.");
+	}
+
+	private static void ForFun(AnimationClip clip, string clipName)
+	{
+		EditorCurveBinding[] curveBindings = AnimationUtility.GetCurveBindings(clip);
+		AnimationClipCurveData[] curves = new AnimationClipCurveData[curveBindings.Length];
+		for (int index = 0; index < curves.Length; ++index)
+		{
+			curves[index] = new AnimationClipCurveData(curveBindings[index]);
+			curves[index].curve = AnimationUtility.GetEditorCurve(clip, curveBindings[index]);
+		}
+		foreach(AnimationClipCurveData curveDate in curves)
+		{
+			var keyFrames = curveDate.curve.keys;
+			for(int i=0;i<keyFrames.Length;i++)
+			{
+				var key = keyFrames[i];
+				key.value = float.Parse(key.value.ToString("f3"));
+				key.inTangent = float.Parse(key.inTangent.ToString("f3"));
+				key.outTangent = float.Parse(key.outTangent.ToString("f3"));
+				keyFrames[i] = key;
+			}
+			curveDate.curve.keys = keyFrames;
+			clip.SetCurve(curveDate.path,curveDate.type,curveDate.propertyName,curveDate.curve);
+		}
+		AssetDatabase.CreateAsset(clip, clipName);
+	}
+}
